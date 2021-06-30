8621618b68ded60093befdaba13ee82c86506534
rstouder <rstouder@intrasect.com>
2021-06-30 13:31:27 -0400

    Add project files.

---
+++ WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WiredBrainCoffee.CustomersApp
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            var messageDialog = new MessageDialog("Customer Added!");
+            await messageDialog.ShowAsync();
+        }
+
+        private void ButtonDeleteCustomer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonMove_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
