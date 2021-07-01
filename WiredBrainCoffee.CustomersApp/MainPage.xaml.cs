075a98da656865ccd77069e37b39b99628d2d7dd
rstouder <rstouder@intrasect.com>
2021-07-01 15:32:53 -0400

    Accessing Objects From Code

--- WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
+++ WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using WiredBrainCoffee.CustomersApp.DataProvider;
+using WiredBrainCoffee.CustomersApp.Model;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -9,21 +13,49 @@ namespace WiredBrainCoffee.CustomersApp
 {
     public sealed partial class MainPage : Page
     {
+        private CustomerDataProvider _customerDataProvider;
+
         public MainPage()
         {
             this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+            App.Current.Suspending += App_SuspendingAsync;
+            _customerDataProvider = new CustomerDataProvider();
         }
 
-        private async void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
-            var messageDialog = new MessageDialog("Customer Added!");
-            await messageDialog.ShowAsync();
+            customerListView.Items.Clear();
+
+            var customers = await _customerDataProvider.LoadCustomersAsync();
+            foreach(var customer in customers)
+            {
+                customerListView.Items.Add(customer);
+            }
+        }
+        private async void App_SuspendingAsync(object sender, Windows.ApplicationModel.SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            await _customerDataProvider.SaveCustomersAsync(
+                customerListView.Items.OfType<Customer>());
+            deferral.Complete();
         }
 
-        private async void ButtonDeleteCustomer_Click(object sender, RoutedEventArgs e)
+        private void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
         {
-            var messageDialog = new MessageDialog("Customer Deleted!");
-            await messageDialog.ShowAsync();
+            var customer = new Customer { FirstName = "New" };
+            customerListView.Items.Add(customer);
+            customerListView.SelectedItem = customer;
+        }
+
+        private void ButtonDeleteCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            var customer = customerListView.SelectedItem as Customer;
+            if(customer != null)
+            {
+                customerListView.Items.Remove(customer);
+            }
         }
 
         private void ButtonMove_Click(object sender, RoutedEventArgs e)
@@ -35,6 +67,35 @@ namespace WiredBrainCoffee.CustomersApp
             Grid.SetColumn(customerListGrid, newColumn);
 
             moveSymbolIcon.Symbol = newColumn == 0 ? Symbol.Forward : Symbol.Back;
+        }
+
+        private void customerListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var customer = customerListView.SelectedItem as Customer;
+            txtFirstName.Text = customer?.FirstName ?? "";
+            txtLastName.Text = customer?.LastName ?? "";
+            chkIsDeveloper.IsChecked = customer?.IsDeveloper;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateCustomer();
+        }
+
+        private void CheckBox_IsCheckedChanged(object sender, RoutedEventArgs e)
+        {
+            UpdateCustomer();
+        }
+
+        private void UpdateCustomer()
+        {
+            var customer = customerListView.SelectedItem as Customer;
+            if (customer != null)
+            {
+                customer.FirstName = txtFirstName.Text;
+                customer.LastName = txtLastName.Text;
+                customer.IsDeveloper = chkIsDeveloper.IsChecked.GetValueOrDefault();
+            }
         }
     }
 }
